3aef8ed7e62bd094a451c5374a43b5ab25ca1c04
Benjamin Charity <ben@benjamincharity.com>
2014-09-08 22:58:38 -0400

    added humans.txt, added icons, added scss files, added bg images

---
+++ app/scripts/directives/setBackgroundImage.js
@@ -0,0 +1,40 @@
+angular.module('bc')
+.directive('setBackgroundImage', [
+  function() {
+    return {
+
+      link: function(scope, element, attrs) {
+
+        var images = [1, 2];
+
+        var isNotMobile = !Modernizr.touch;
+
+        // get a random image number from our array
+        var imageNumber = images[Math.floor(Math.random()*images.length)];
+
+        // set the jpg as our background
+        element.css(
+          { 'background-image': 'url(images/bg/' + imageNumber + '.jpg)' }
+        );
+
+        // if we are not on a mobile device, preload the gif
+        if( isNotMobile ) {
+          // create a new image for the gif
+          var img = new Image();
+
+          // build the url and assign to the new image to begin preloading
+          img.src = 'images/bg/' + imageNumber + '.gif';
+
+          img.onload = function() {
+            element.css(
+              { 'background-image': 'url(images/bg/' + imageNumber + '.gif)' }
+            );
+          }
+        }
+
+
+      }
+
+    }
+  }
+]);
--- app/scripts/services/contentService.js
+++ app/scripts/services/contentService.js
@@ -1,5 +1,3 @@
-'use strict';
-
 angular.module('bc')
 .factory( 'contentService', function($http, $window) {
 
