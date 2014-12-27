1339b552926d5c8d368b2d531a3a7d972f1d3ce5
Benjamin Charity <ben@benjamincharity.com>
2014-12-27 15:04:29 -0500

    added utilities scss, finalized page transitions, layout markup finalized, logo movement finalized

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,5 +2,27 @@
 
 angular.module('bc', ['ngAnimate', 'ui.router'])
 .run(function($rootScope) {
+
   $rootScope.loaderIsVisible = true;
+
+
+
+
+  //////////////////////////////////////////////////
+  //
+  // Change the logo size when not on the landing page
+  //
+  //////////////////////////////////////////////////
+
+  $rootScope.$on('$stateChangeStart',
+  function(event, toState){
+
+    if( toState.name === 'landing' ) {
+      $rootScope.headerIsCompact = false;
+    } else {
+      $rootScope.headerIsCompact = true;
+    }
+
+  });
+
 });
--- app/scripts/routes.js
+++ app/scripts/routes.js
@@ -10,42 +10,34 @@ angular.module('bc').config(function($stateProvider, $urlRouterProvider) {
 
     //
     // Home
-    //
     .state('landing', {
-      url: "/",
-      //abstract: true,
-      templateUrl: "/partials/landing.html",
-      controller: "AppCtrl"
+      url: '/',
+      controller: 'AppCtrl'
     })
 
     //
     // Projects
-    //
-    .state('landing.projects', {
-      url: "projects",
-      templateUrl: "/partials/projects.html",
-      controller: "ProjectsCtrl"
+    .state('projects', {
+      url: '/projects',
+      templateUrl: '/partials/projects.html',
+      controller: 'ProjectsCtrl'
     })
 
     //
     // About
-    //
-    .state('landing.about', {
-      url: "about",
-      templateUrl: "/partials/about.html",
-      controller: "AboutCtrl"
+    .state('about', {
+      url: '/about',
+      templateUrl: '/partials/about.html',
+      controller: 'AboutCtrl'
     })
 
     //
     // Contact
-    //
-    .state('landing.contact', {
-      url: "contact",
-      templateUrl: "/partials/contact.html",
-      controller: "ContactCtrl"
+    .state('contact', {
+      url: '/contact',
+      templateUrl: '/partials/contact.html',
+      controller: 'ContactCtrl'
     })
-
-
 
     ;
 });
