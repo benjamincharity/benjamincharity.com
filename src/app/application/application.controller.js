a7eaa865ef96352f217aa086883c16f5d9a5c030
Benjamin Charity <ben@benjamincharity.com>
2016-03-16 22:56:25 -0400

    header really working

--- src/app/application/application.controller.js
+++ src/app/application/application.controller.js
@@ -25,29 +25,36 @@ export class ApplicationController {
         ];
 
 
-        // Toggle the logo/header size
-        // TODO: Move to route data object
         const stateChangeStart = this.$rootScope.$on('$stateChangeStart', (event, toState) => {
-
-            // If no data object exists, bail out
-            if (!toState.data) {
-                return false;
-            }
-
-            // If a view has a header state defined
-            if (toState.data.compactHeader) {
-                this.compactHeader = toState.data.compactHeader;
-            }
-
-            // If a view has a title
-            if (toState.data.viewTitle) {
-                this.title = 'Benjamin Charity - ' + toState.data.viewTitle;
-            } else {
-                this.title = 'Benjamin Charity';
-            }
-
+            this._handleStateChange.call(this, event, toState);
         });
 
+    }
+
+
+    /**
+     * Perform tasks on each state change
+     *
+     * @param {Object} event
+     * @param {Object} toState
+     */
+    _handleStateChange(event, toState) {
+        // If no data object exists, bail out
+        if (!toState.data) {
+            return false;
+        }
+
+        // If a view has a header state defined
+        if (typeof(toState.data.compactHeader) === 'boolean') {
+            this.compactHeader = toState.data.compactHeader;
+        }
+
+        // If a view has a title
+        if (toState.data.viewTitle) {
+            this.title = 'Benjamin Charity - ' + toState.data.viewTitle;
+        } else {
+            this.title = 'Benjamin Charity';
+        }
     }
 
 
