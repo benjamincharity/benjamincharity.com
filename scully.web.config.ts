5849b3cb5f5bce0df6b82bc37e5f1cc7971d17a4
Benjamin Charity <ben@benjamincharity.com>
2021-06-03 19:43:21 -0400

    🚧 WIP: update yarn lock [ci skip] 🚧

--- scully.web.config.ts
+++ scully.web.config.ts
@@ -1,13 +1,14 @@
 import { ScullyConfig, setPluginConfig } from '@scullyio/scully';
 import { timeToRead, timeToReadOptions } from 'scully-plugin-time-to-read';
 
-// setPluginConfig('md', { enableSyntaxHighlighting: true });
-//
-// setPluginConfig(timeToRead, {
-//   path: '/articles',
-// } as timeToReadOptions);
+setPluginConfig('md', { enableSyntaxHighlighting: true });
+
+setPluginConfig(timeToRead, {
+  path: '/articles',
+} as timeToReadOptions);
 
 export const config: ScullyConfig = {
+  projectRoot: './apps/web/src',
   projectName: 'web',
   outDir: './dist/static',
   distFolder: './dist/web/browser',
