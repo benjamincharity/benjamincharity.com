d4548062349385064095dfd53ccae2add4065d61
Benjamin Charity <ben@benjamincharity.com>
2021-06-03 22:35:53 -0400

    🚧 WIP: [ci skip] 🚧

--- apps/web/src/app/shared/highlight.service.ts
+++ apps/web/src/app/shared/highlight.service.ts
@@ -11,17 +11,15 @@ import 'prismjs';
 import 'prismjs/plugins/toolbar/prism-toolbar';
 import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard';
 import 'prismjs/plugins/diff-highlight/prism-diff-highlight';
-import 'prismjs/plugins/diff-highlight/prism-diff-highlight';
 import 'prismjs/plugins/line-numbers/prism-line-numbers';
 
 // Styles
 import 'prismjs/components/prism-bash';
 import 'prismjs/components/prism-css';
+import 'prismjs/components/prism-diff';
 import 'prismjs/components/prism-javascript';
 import 'prismjs/components/prism-json';
 import 'prismjs/components/prism-markdown';
-import 'prismjs/components/prism-ruby';
-import 'prismjs/components/prism-sass';
 import 'prismjs/components/prism-scss';
 import 'prismjs/components/prism-typescript';
 import 'prismjs/components/prism-vim';
